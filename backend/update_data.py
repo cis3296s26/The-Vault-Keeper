9b32c35f653a7e299ad9cd98ac8d52dbbde95527
Cole Roberts <tup86300@temple.edu>
2026-04-07 20:18:24 -0400

    Created and tested db. Messing around with FastAPI.

---
+++ backend/update_data.py
@@ -0,0 +1,17 @@
+import os
+import psycopg
+from dotenv import load_dotenv
+load_dotenv()
+conn_string = os.getenv("DATABASE_URL_DEV")
+try:
+    with psycopg.connect(conn_string) as conn:
+        print("Connection established")
+        with conn.cursor() as cur:
+            # Update a data row in the table
+            cur.execute(
+                "UPDATE books SET in_stock = %s WHERE title = %s;", (True, "Dune")
+            )
+            print("Updated stock status for 'Dune'.")
+except Exception as e:
+    print("Connection failed.")
+    print(e)
