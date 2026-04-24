9b2063820fbfadefba71d9cc8ec49ce72f882afb
osele-jp <145854894+osele-jp@users.noreply.github.com>
2026-04-23 22:03:17 -0400

    Fixed some network issues

--- frontend/src/api/files.js
+++ frontend/src/api/files.js
@@ -1,5 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_URL || 
-  `${window.location.protocol}//${window.location.hostname}:8000`
+const BASE_URL =  `${window.location.protocol}//${window.location.hostname}:8000`
 
 export const getFiles = async () => {
     const res = await fetch(`${BASE_URL}/api/files/`)
